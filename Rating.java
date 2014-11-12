3a14453ea84702ec1c7016e95e3d6f6b8fd28555
Leonardo Invernizzi <leo.invernizzi@hotmail.com>
2014-11-11 22:01:15 -0200

    Update Rating.java

--- Rating.java
+++ Rating.java
@@ -2,27 +2,38 @@ package javaapplication1;
 
 import java.io.Serializable;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author henrique
- */
-public class Rating implements Serializable
-{
-    
-    private int usuario_id;
-    private long isbn;
-    private int rating;
-    
-    public Rating(int usuario_id, long isbn, int rating)
-    {
-        this.usuario_id = usuario_id;
-        this.isbn = isbn;
-        this.rating = rating;
-    }
-    
+public class Rating implements Serializable, Comparable<Rating> {
+
+	private static final long serialVersionUID = 1L;
+	private int usuario_id;
+	private long isbn;
+	private int rating;
+
+	public Rating(int usuario_id, long isbn, int rating) {
+		this.setUsuario_id(usuario_id);
+		this.setIsbn(isbn);
+		this.rating = rating;
+	}
+
+	@Override
+	public int compareTo(Rating rating) {
+		return this.rating - rating.rating;
+	}
+
+	public int getUsuario_id() {
+		return usuario_id;
+	}
+
+	public void setUsuario_id(int usuario_id) {
+		this.usuario_id = usuario_id;
+	}
+
+	public long getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+
 }
