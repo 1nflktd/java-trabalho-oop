b048a8b8b7432e93b73c361e7d678fb67dbcfd2c
Leonardo Invernizzi <leo.invernizzi@hotmail.com>
2014-11-11 22:01:36 -0200

    Update SerializarClasse.java

--- SerializarClasse.java
+++ SerializarClasse.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package javaapplication1;
 
 import java.io.FileInputStream;
@@ -13,24 +7,22 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 
-/**
- *
- * @author UCS
- */
 public class SerializarClasse {
     
-    public void serializarLista(ArrayList lista, String caminho) throws IOException
+    public void serializarLista(ArrayList<?> lista, String caminho) throws IOException
     {
-        ObjectOutputStream streamObjFlivros = new ObjectOutputStream(new FileOutputStream(caminho));
-        streamObjFlivros.writeObject(lista);
+        ObjectOutputStream streamObj = new ObjectOutputStream(new FileOutputStream(caminho));
+        streamObj.writeObject(lista);
+        streamObj.close();
     }
     
-    public ArrayList<Livro> lerObj(String caminho) throws IOException, ClassNotFoundException
+    public void lerObj(ArrayList<?> aLista, String caminho) throws IOException, ClassNotFoundException
     {
-        ArrayList<Livro> aLivros = new ArrayList<>();
         ObjectInputStream stream = new ObjectInputStream(new FileInputStream(caminho));
-        aLivros = (ArrayList<Livro>) stream.readObject();
-        return aLivros;
+        aLista = (ArrayList<?>) stream.readObject();
+        stream.close();
+        //return aLista;
     }
+
 
 }
