f0018811420e2595ef0a193d62764aef8e312553
Leonardo Invernizzi <leo.invernizzi@hotmail.com>
2014-11-17 22:19:25 -0200

    Update Livro.java

--- Livro.java
+++ Livro.java
@@ -8,6 +8,8 @@ public class Livro implements Serializable, Comparable<Livro>
 	private static final long serialVersionUID = 1L;
 	private long isbn;
 	private String titulo;
+    private int somaRating;
+    private int qtdeRating;
 
 	public Livro()
 	{
@@ -18,8 +20,22 @@ public class Livro implements Serializable, Comparable<Livro>
 	{
 		this.setIsbn(isbn);
 		this.setTitulo(titulo);
+        somaRating = 0;
+        qtdeRating = 0;
 	}
 
+    public void addSoma(int valor) {
+        somaRating += valor;
+    }
+    
+    public void addQtde() {
+        qtdeRating++;
+    }
+    
+    public double getMedia() {
+        return somaRating / qtdeRating;
+    }
+    
 	public long getIsbn() {
 		return isbn;
 	}
@@ -35,7 +51,7 @@ public class Livro implements Serializable, Comparable<Livro>
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
-
+    
 	@Override
 	public int compareTo(Livro livro) {
 		return this.titulo.toUpperCase().compareTo(livro.titulo.toUpperCase());
