6c8224a20e5c84d24f9ea6d57f5120acf1ab40da
Leonardo Invernizzi <leo.invernizzi@hotmail.com>
2014-11-17 22:19:51 -0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -17,11 +17,32 @@ public class Main {
 
 		SerializarClasse serClasse = new SerializarClasse();
 
-		aLivros = (ArrayList<Livro>) serClasse.<Livro>lerObj("C:/Users/Henrique/Documents/listaLivro.obj");
-		aUsuario = (ArrayList<Usuario>) serClasse.<Usuario>lerObj("C:/Users/Henrique/Documents/listaUsuario.obj");
-		aRating = (ArrayList<Rating>) serClasse.<Rating>lerObj("C:/Users/Henrique/Documents/listaRating.obj");
+		aLivros = (ArrayList<Livro>) serClasse.<Livro>lerObj("C:/Users/UCS/Documents/listaLivro.obj");
+		aUsuario = (ArrayList<Usuario>) serClasse.<Usuario>lerObj("C:/Users/UCS/Documents/listaUsuario.obj");
+		aRating = (ArrayList<Rating>) serClasse.<Rating>lerObj("C:/Users/UCS/Documents/listaRating.obj");
 	}
 	
+    public static void carregarRatingUsuario()
+    {
+        for (Rating r : aRating) {
+            for (Usuario user : aUsuario) {
+                if (r.getUsuario_id() == user.getId()) {
+                    user.addLista(r);
+                    break;
+                }
+            }
+            for (Livro livro : aLivros) {
+                if (livro.getIsbn() == r.getIsbn()) {
+                    livro.addQtde();
+                    livro.addSoma(r.getRating());
+                }
+            }
+            /*Usuario user = new Usuario();
+            aUsuario.
+            user.getUsuarioPorId(r.getUsuario_id());;*/
+        }
+    }
+    
 	public static void carregar()
 	{
 		System.out.println("Carregar");
@@ -32,10 +53,22 @@ public class Main {
 			aUsuario = obj.carregarUsuarios();
 			aRating = obj.carregarRating();
 
+            try{
+                //Ordenacao.mergeSort(aLivros);
+                Ordenacao.mergeSort(aUsuario);
+                Ordenacao.mergeSort(aRating);
+            } catch (Exception e) {
+                System.out.println("Problema ao ordenar: " + e.getMessage());
+            }
+                
+            carregarRatingUsuario();
+            
+            System.out.println(aUsuario.get(0));
+            
 			SerializarClasse serializar = new SerializarClasse();
-			serializar.serializarLista(aLivros, "C:/Users/Henrique/Documents/listaLivro.obj");
-			serializar.serializarLista(aUsuario, "C:/Users/Henrique/Documents/listaUsuario.obj");
-			serializar.serializarLista(aRating, "C:/Users/Henrique/Documents/listaRating.obj");
+			serializar.serializarLista(aLivros, "C:/Users/UCS/Documents/listaLivro.obj");
+			serializar.serializarLista(aUsuario, "C:/Users/UCS/Documents/listaUsuario.obj");
+			serializar.serializarLista(aRating, "C:/Users/UCS/Documents/listaRating.obj");
 		} catch (IOException e) {
 			System.out.println(e);
 		}
@@ -47,23 +80,32 @@ public class Main {
 		} catch (Exception e) {
 			carregar();
 		}
-		
-		System.out.println("Ordenar Livros");
-		Ordenacao.mergeSort(aLivros);
-		for	(Livro livro : aLivros) {
-			System.out.println(livro.getTitulo());
-		}
-		
-		System.out.println("Ordenar Usuarios");
-		Ordenacao.mergeSort(aUsuario);
-		for	(Usuario usuario : aUsuario) {
-			System.out.println(usuario.getCountry());
-		}
-		
-		System.out.println("Ordenar Ratings");
-		Ordenacao.mergeSort(aRating);
-		for	(Rating rating : aRating) {
-			System.out.println(rating.getRating());
-		}
+		/*
+        try{
+            System.out.println("Ordenar Livros");
+            Ordenacao.mergeSort(aLivros);
+            
+            for	(Livro livro : aLivros) {
+                System.out.println(livro.getTitulo());
+            }
+
+            System.out.println("Ordenar Usuarios");
+            Ordenacao.mergeSort(aUsuario);
+            for	(Usuario usuario : aUsuario) {
+                System.out.println(usuario.getCountry());
+            }
+
+            System.out.println("Ordenar Ratings");
+            Ordenacao.mergeSort(aRating);
+            for	(Rating rating : aRating) {
+                System.out.println(rating.getRating());
+            }
+        } catch (Exception e) {
+            System.out.println("Problema ao ordenar: " + e.getMessage());
+        }
+        */
+        
+        
+        
 	}
 }
