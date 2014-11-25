2c4357a8bab7ceff5b55d887294796bf8567fa6c
Leonardo Invernizzi <leo.invernizzi@hotmail.com>
2014-11-24 22:16:04 -0200

    Update SerializarClasse.java

--- SerializarClasse.java
+++ SerializarClasse.java
@@ -7,7 +7,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 public class SerializarClasse {
     
